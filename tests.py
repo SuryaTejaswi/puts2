9d2a4041596f5e70c552e71345f90773293c0c5f
SuryaTejaswi <triparagirisuryatejaswi@gmail.com>
2020-05-29 17:18:28 +0200

    Add division module including tests

--- tests.py
+++ tests.py
@@ -15,6 +15,11 @@ class OnlineCalculatorTestCase(unittest.TestCase):
         response = self.app.get('/')
         self.assertEqual(b'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n', response.data)
 
+    def test_division(self):
+        """Test for division"""
+        response = self.app.get('/div?A=6&B=3')
+        self.assertEqual(b'2 \n', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
