087afb9f0a6a34bdeb8e49c2ebeec511fc02620f
SuryaTejaswi <triparagirisuryatejaswi@gmail.com>
2020-05-29 20:03:40 +0200

    Add more tests for addition module

--- tests.py
+++ tests.py
@@ -19,6 +19,10 @@ class OnlineCalculatorTestCase(unittest.TestCase):
         """Test for addition resource"""
         response = self.app.get('/add?A=5&B=3')
         self.assertEqual(b'8 \n', response.data)
+        response = self.app.get('/add?A=6&B=3/5')
+        self.assertEqual(b'6.600 \n', response.data)
+        response = self.app.get('/add?A=0&B=-9292')
+        self.assertEqual(b'-9292 \n', response.data)
 
 
 if __name__ == '__main__':
