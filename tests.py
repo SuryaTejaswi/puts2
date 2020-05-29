079d2787db792a65893729ba21e50a5210f6cecd
SuryaTejaswi <triparagirisuryatejaswi@gmail.com>
2020-05-29 16:55:27 +0200

    Add tests file

---
+++ tests.py
@@ -0,0 +1,20 @@
+import unittest
+import main
+
+
+class OnlineCalculatorTestCase(unittest.TestCase):
+    """Test features of online calculator"""
+
+    def setUp(self):
+        """Setup """
+        main.app.testing = True
+        self.app = main.app.test_client()
+
+    def test_no_resources(self):
+        """Test for no route"""
+        response = self.app.get('/')
+        self.assertEqual(b'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
