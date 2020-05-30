3e6cab4dc9249c68385581d2ba5887d21d853468
SuryaTejaswi <triparagirisuryatejaswi@gmail.com>
2020-05-30 16:20:36 +0200

    changes made

--- tests.py
+++ tests.py
@@ -15,36 +15,33 @@ class OnlineCalculatorTestCase(unittest.TestCase):
         response = self.app.get('/add?A=5&B=3')
         self.assertEqual(b'8.0 \n', response.data)
         response = self.app.get('/add?A=6&B=3/5')
-        self.assertEqual(b'6.6000 \n', response.data)
+        self.assertEqual(b'6.6 \n', response.data)
         response = self.app.get('/add?A=0&B=-9292')
         self.assertEqual(b'-9292.0 \n', response.data)
-        # integer numbers testing
-        response = self.app.get('/add?A=5&B=3')
-        self.assertEqual(b'8 \n', response.data)
 
         # rational numbers testing
         response = self.app.get('/add?A=5/3&B=3/4')
-        self.assertEqual(b'2.417 \n', response.data)
+        self.assertEqual(b'2.4167 \n', response.data)
 
         # when both A and B are both floats
         response = self.app.get('/add?A=5.4&B=3.4678')
-        self.assertEqual(b'8.868 \n', response.data)
+        self.assertEqual(b'8.8678 \n', response.data)
 
         # when A is an int and B is float
         response = self.app.get('/add?A=5&B=-3.4678')
-        self.assertEqual(b'1.532 \n', response.data)
+        self.assertEqual(b'1.5322 \n', response.data)
 
         # when A is a float and B is an int
         response = self.app.get('/add?A=-3.4678&B=5')
-        self.assertEqual(b'1.532 \n', response.data)
+        self.assertEqual(b'1.5322 \n', response.data)
 
         # when A is a fraction and B is an int
         response = self.app.get('/add?A=3/4&B=5')
-        self.assertEqual(b'5.750 \n', response.data)
+        self.assertEqual(b'5.75 \n', response.data)
 
         # when A is an int and B is a fraction
         response = self.app.get('/add?A=5&B=3/4')
-        self.assertEqual(b'5.750 \n', response.data)
+        self.assertEqual(b'5.75 \n', response.data)
 
         # corner cases testing
         # when A = x/0 where x belongs to any integer
@@ -65,7 +62,7 @@ class OnlineCalculatorTestCase(unittest.TestCase):
 
         # Handling of POST Method 
         response = self.app.post('/add?A=1&B=2)')
-        self.assertEqual(b"3 \n", response.data)
+        self.assertEqual(b'B should be a number (includes integers, rationals, float)! \n', response.datagit add )
 
 
 if __name__ == '__main__':
