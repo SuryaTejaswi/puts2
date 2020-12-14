326af871dd9b7e4e3c8053f49900d21d776ab5a5
SuryaTejaswi <triparagirisuryatejaswi@gmail.com>
2020-12-14 21:12:07 +0100

    added main.py in average

--- main.py
+++ main.py
@@ -1,90 +1,47 @@
-from fractions import Fraction
 
 from flask import Flask, request
+from fractions import Fraction
+import statistics
 
 app = Flask(__name__)
 
 
-def inputs():
-    if request.method == 'POST':
-        value1 = request.values.get('A', default=0, type=str)
+def input():
+    if request.method != 'GET':
+        a = request.values.get('X', default=0, type=str)
+
     else:
-        value1 = request.args.get('A', default=0, type=str)
+
+        a = request.args.get('X', default=0, type=str)
     try:
-        value1 = Fraction(value1)
-    except ZeroDivisionError:
-        return "A's denominator shouldn't be 0! \n"
+        nw = []
+        for val in a.split(','):
+            nw.append(Fraction(val))
+
     except ValueError:
-        return "A should be a number (includes integers, rationals, float)! \n"
-    if request.method == 'GET':
-        value2 = request.args.get('B', default=0, type=str)
+        warning = "Enter a valid input. "
+        return warning
+
+    return nw
+
+
+@app.route('/mean', methods=['GET', 'POST'])
+@app.route('/average', methods=['GET', 'POST'])
+@app.route('/avg', methods=['GET', 'POST'])
+def mean():
+    try:
+        nw = input()
+        r = statistics.mean(nw)
+    except ValueError:
+        warning = input()
+        return warning
     else:
-        value2 = request.values.get('B', default=0, type=str)
-    try:
-        value2 = Fraction(value2)
-    except ValueError:
-        return "B should be a number (includes integers, rationals, float)! \n"
-    except ZeroDivisionError:
-        return "B's denominator shouldn't be 0! \n"
-    return value1, value2
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
-
-
-@app.route('/add', methods=['GET', 'POST'])
-def addition():
-    try:
-        value1, value2 = inputs()
-        result = value1 + value2
-    except ValueError:
-        error_msg = inputs()
-        return error_msg
-    else:
-        return str(round(float(result), 4)) + ' \n'
-
-
-@app.route('/sub', methods=['GET', 'POST'])
-def subtraction():
-    try:
-        value1, value2 = inputs()
-        result = value1 - value2
-    except ValueError:
-        error_msg = inputs()
-        return error_msg
-    else:
-        return str(round(float(result), 4)) + '\n'
-
-
-@app.route('/mul', methods=['GET', 'POST'])
-def multiplication():
-    try:
-        value1, value2 = inputs()
-        result = value1 * value2
-    except ValueError:
-        error_msg = inputs()
-        return error_msg
-    else:
-        return str(round(float(result), 4)) + '\n'
-
-
-@app.route('/div', methods=['GET', 'POST'])
-def division():
-    try:
-        value1, value2 = inputs()
-        try:
-            result = (value1 / value2)
-        except ZeroDivisionError:
-            error_msg = "B's value shouldn't be zero! \n"
-            return error_msg
-    except ValueError:
-        warning_msg = inputs()
-        return warning_msg
-    else:
-        return str(round(float(result), 4)) + '\n'
+        if float(r).is_integer():
+            answer = int(r)
+            return "%d \n" % answer
+        else:
+            return str(float(round(r, 3))) + " \n"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
